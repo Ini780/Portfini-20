83f692bdffdc338e4540d63ccf99443cd8469d86
Ini780 <118449766+Ini780@users.noreply.github.com>
2023-07-10 07:48:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let fishy= document.querySelector(".fish")
+let fishyPath= document.querySelector(".fishes")
+
+fishy.onclick=function(){
+  fishyPath.style.display="block";
+}
+
+//slideshow on hobbies//
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
